f14e90b781a72fcf0dea85e87bc3872a33c2bbeb
Jeff <Jeff@Jeff-Laptop>
2014-01-25 08:57:31 -0600

    one more thing to fix and we should be driving

--- AerialAssist/src/edu/wpi/first/wpilibj/templates/commands/MecanumDrive.java
+++ AerialAssist/src/edu/wpi/first/wpilibj/templates/commands/MecanumDrive.java
@@ -20,13 +20,10 @@ public class MecanumDrive extends CommandBase {
     }
 
     protected void execute() {
-        //todo:   COMPLETE?
+       
         //here we already have to access the io object. 
         //read the up/down and left/right from io.
-        //then pass those values to the mecanum drive funtion. 
-        //remember you'll need to read both the left and right triggers
-        //I would just add them to gether to get a final rotation value
-        //make one negative for a counter clockwise rotation
+        //then pass those values to the mecanum drive function. 
         driveTrain.mecanumDrive(oi.getXbox().getX(GenericHID.Hand.kLeft),
                                 oi.getXbox().getY(GenericHID.Hand.kLeft),
                                 oi.getXbox().getThrottle());
