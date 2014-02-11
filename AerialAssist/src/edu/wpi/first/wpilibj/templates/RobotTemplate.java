683b446af8530603f529c38fee0eca38f8e285e8
Team3946 GeForce <Team3946 GeForce@Team3946GeForce.stpsb.pri>
2014-02-10 18:01:24 -0600

    Merge origin/master
    Conflicts:
    	AerialAssist/nbproject/project.xml

--- AerialAssist/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ AerialAssist/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
+import edu.wpi.first.wpilibj.templates.commands.Autonomous;
 import edu.wpi.first.wpilibj.templates.commands.CommandBase;
 import edu.wpi.first.wpilibj.templates.commands.ExampleCommand;
 import edu.wpi.first.wpilibj.templates.commands.StartCompressor;
@@ -35,7 +36,7 @@ public class RobotTemplate extends IterativeRobot {
      */
     public void robotInit() {
         // instantiate the command used for the autonomous period
-        autonomousCommand = new ExampleCommand();
+        autonomousCommand = new Autonomous();
         compressor = new StartCompressor();
 
         // Initialize all subsystems
