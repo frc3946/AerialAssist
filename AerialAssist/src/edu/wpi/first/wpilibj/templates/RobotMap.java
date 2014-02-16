05b06d200030b6c0cc6c0afee6d90b73d4bfc568
Allister Wright <allister.wright@gmail.com>
2014-02-16 15:49:46 -0600

    Need functional Solenoid wires to test full teleop control.  all other components functional.  Solenoid module properly lights up.

--- AerialAssist/src/edu/wpi/first/wpilibj/templates/RobotMap.java
+++ AerialAssist/src/edu/wpi/first/wpilibj/templates/RobotMap.java
@@ -7,46 +7,47 @@ package edu.wpi.first.wpilibj.templates;
  * floating around.
  */
 public class RobotMap {
-    
-     // PWM:
-     public static final int fLeft = 1;
-     public static final int fRight = 2;
-     public static final int bRight = 3;
-     public static final int bLeft = 4;
-     public static final int bootMotor1 = 5;
-     public static final int bootMotor2 = 6;
-     
-     // Relays
-     public static final int compressor = 1;
-     public static final int loadFingers = 2;
-     
-     // Solenoids:
-     public static final int pG = 1;
-     public static final int xG = 2;
-     public static final int pW = 3;
-     public static final int xW = 4;
-     
-     // Digital:
-     public static final int pressureSwitch = 1;
-     
-     // Analog:
-     public static final int gyro = 1;
-     public static final int bootSensor = 2;
-     public static final int knob = 3;
-     
-     public static final int xboxController = 1;
-     public static final int kickBall = XboxController.ButtonType.kX.value;
-     public static final int raiseArm = XboxController.ButtonType.kL.value;
-     public static final int lowerArm = XboxController.ButtonType.kR.value;
-     public static final int grabBall = XboxController.ButtonType.kY.value;
-     public static final int releaseBall = XboxController.ButtonType.kA.value;
-     public static final int autoAim = XboxController.ButtonType.kB.value;
-     public static final int adjustSpeed = XboxController.ButtonType.kLeftStick.value;
-     public static final int adjustTime = XboxController.ButtonType.kRightStick.value;
-     
-     //Pi Data
-     public static int offset = 0;
-     public static int distance = 0;
-     public static String found = " ";
-     public static String hot = " ";
+
+    // PWM:
+    public static final int fLeft = 1;
+    public static final int fRight = 2;
+    public static final int bRight = 3;
+    public static final int bLeft = 4;
+    public static final int bootMotor1 = 5;
+    public static final int bootMotor2 = 6;
+
+    // Relays
+    public static final int compressor = 1;
+    public static final int loadFingers = 2;
+
+    // Solenoids:
+    public static final int pG = 1;
+    public static final int xG = 2;
+    public static final int pW = 3;
+    public static final int xW = 4;
+
+    // Digital:
+    public static final int pressureSwitch = 1;
+
+    // Analog:
+    public static final int gyro = 1;
+    public static final int bootSensor = 2;
+    public static final int knob = 3;
+
+    //Controllers:
+    public static final int xboxController = 1;
+    public static final int kickBall = XboxController.ButtonType.kX.value;
+    public static final int raiseArm = XboxController.ButtonType.kR.value;
+    public static final int lowerArm = XboxController.ButtonType.kL.value;
+    public static final int grabBall = XboxController.ButtonType.kY.value;
+    public static final int releaseBall = XboxController.ButtonType.kA.value;
+    public static final int autoAim = XboxController.ButtonType.kB.value;
+    public static final int adjustSpeed = XboxController.ButtonType.kLeftStick.value;
+    public static final int adjustTime = XboxController.ButtonType.kRightStick.value;
+
+    //Pi Data
+    public static int offset = 0;
+    public static int distance = 0;
+    public static String found = " ";
+    public static String hot = " ";
 }
