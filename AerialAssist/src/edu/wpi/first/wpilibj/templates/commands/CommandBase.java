f84f0927649a63d5a23c22c58965768bf49f2352
Team3946 GeForce <Team3946 GeForce@Team3946GeForce.stpsb.pri>
2014-03-24 18:47:49 -0500

    Merge origin/master
    Conflicts:
    	AerialAssist/nbproject/project.bak.xml
    	AerialAssist/nbproject/project.xml
    	AerialAssist/src/edu/wpi/first/wpilibj/templates/commands/AutoAim.java
    	AerialAssist/src/edu/wpi/first/wpilibj/templates/commands/CommandBase.java
    	AerialAssist/src/edu/wpi/first/wpilibj/templates/commands/KickBall.java
    	AerialAssist/src/edu/wpi/first/wpilibj/templates/commands/MoveBoot.java
    	AerialAssist/src/edu/wpi/first/wpilibj/templates/commands/StopBoot.java

--- AerialAssist/src/edu/wpi/first/wpilibj/templates/commands/CommandBase.java
+++ AerialAssist/src/edu/wpi/first/wpilibj/templates/commands/CommandBase.java
@@ -22,6 +22,7 @@ public abstract class CommandBase extends Command {
     public static Gyro gyro = new Gyro(RobotMap.gyro);
     public static LoadingArm loadingArm = new LoadingArm();
     public static LoadingFingers loadFingers = new LoadingFingers();
+    public static FunLights funLights = new FunLights();
 //    public static RaspberryPi raspberryPi = new RaspberryPi();
 //    public static ThreadedberryPi threadedberryPi = new ThreadedberryPi();
 //   public static CatapultMotor catapult = new CatapultMotor();
